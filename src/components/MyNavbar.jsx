4bdc4dc52f6808663ff99628b183ef9940682aa0
MaurizioCr <crispinomaurizio034@gmail.com>
2025-03-25 19:14:20 +0100

    NavBar

---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,16 @@
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand as={Link} to="/">Pokédex</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link as={Link} to="/">Home</Nav.Link>
+        <Nav.Link as={Link} to="/types">Tipi</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+}
+
+export default NavBar;
