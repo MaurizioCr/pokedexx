0d1c2a948863aade40e6e7b06a903666714528c3
MaurizioCr <crispinomaurizio034@gmail.com>
2025-04-01 23:27:47 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/MyNavbar';
+import MyNavbar from './components/MyNavbar';
 import Home from './components/Home';
-import TypePage from './components/TypePage';
-import PokemonDetail from './components/PokemonDetail';
+import PokemonList from './components/PokemonList';
 import TypeList from './components/TypeList';
-
+import TypePage from './components/TypePage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
-      <NavBar />
+      <MyNavbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/pokemon" element={<PokemonList />} />
         <Route path="/types" element={<TypeList />} />
         <Route path="/type/:type" element={<TypePage />} />
-        <Route path="/pokemon/:id" element={<PokemonDetail />} />
       </Routes>
     </Router>
   );
 }
+
 export default App;
